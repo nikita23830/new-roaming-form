69a1837bc692d2290f8ce69142fb7e2fe332d3eb
msJetBlackHeart <ms.melnikovairina@gmail.com>
2019-07-17 17:59:56 +0300

    фио не прыгает

--- src/Components/name-and-fio.js
+++ src/Components/name-and-fio.js
@@ -1,55 +1,60 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import { Field } from "react-final-form";
-
-import { StyledGrid } from '../StyledComponents/step/'
+import { StyledGrid } from "../StyledComponents/step/";
 
 export const NameAndFio = props => {
-  const { typeUL, disable, insertkey, component } = props
+  const { typeUL, disable, insertkey, component } = props;
 
   return (
     <>
-      {typeUL && <StyledGrid item xs={12} sm={12}>
-        <Field
-          disabled={disable}
-          required={!disable}
-          fullWidth
-          name={`${insertkey}.name`}
-          label='Название организации'
-          component={component}
-        />
-      </StyledGrid>}
-      {!typeUL && <StyledGrid container spacing={1}>
-        <Grid item xs={12} sm={4}>
-          <Field
-            disabled={disable}
-            required={!disable}
-            fullWidth
-            name={`${insertkey}.lastname`}
-            label='Имя'
-            component={component}
-          />
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Field
-            disabled={disable}
-            required={!disable}
-            fullWidth
-            name={`${insertkey}.firstname`}
-            label='Фамилия'
-            component={component}
-          />
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Field
-            disabled={disable}
-            fullWidth
-            name={`${insertkey}.patronymic`}
-            label='Отчество'
-            component={component}
-          />
-        </Grid>
-      </StyledGrid>}
+      {typeUL && (
+        <StyledGrid container spacing={1}>
+          <Grid item xs={12}>
+            <Field
+              disabled={disable}
+              required={!disable}
+              fullWidth
+              name={`${insertkey}.name`}
+              label="Название организации"
+              component={component}
+            />
+          </Grid>
+        </StyledGrid>
+      )}
+      {!typeUL && (
+        <StyledGrid container spacing={1}>
+          <Grid item xs={12} sm={4}>
+            <Field
+              disabled={disable}
+              required={!disable}
+              fullWidth
+              name={`${insertkey}.lastname`}
+              label="Имя"
+              component={component}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Field
+              disabled={disable}
+              required={!disable}
+              fullWidth
+              name={`${insertkey}.firstname`}
+              label="Фамилия"
+              component={component}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Field
+              disabled={disable}
+              fullWidth
+              name={`${insertkey}.patronymic`}
+              label="Отчество"
+              component={component}
+            />
+          </Grid>
+        </StyledGrid>
+      )}
     </>
-  )
-}
+  );
+};
